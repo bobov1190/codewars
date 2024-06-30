3703af6defe20fb2fc4b40b4dfccbc3a2084ccf4
Bryan <bobov1190@gmail.com>
2024-06-30 22:21:17 +0500

    updated the text

--- lovefunc.js
+++ lovefunc.js
@@ -12,7 +12,14 @@ function lovefunc(flower1, flower2) {
     if (flower1 % 2 == 0 && flower2 % 2 == 0) {
         return !inLove;
     }
+    else if (flower1 % 2 != 0 && flower2 % 2 != 0) {
+        return !inLove;
+    }
     else {
         return inLove;
     }
 }
+
+// function lovefunc(flower1, flower2) {
+//    return ((flower1 % 2 == 0 && flower2 % 2 != 0) || (flower1 % 2 != 0 && flower2 % 2 == 0)) ? true : false;
+// }
